28a89e60b51adb8d4c63d0081aba25c5f6e829ed
kevinopp <plata.kevin@uabc.edu.mx>
2022-09-01 10:38:17 -0700

    Terminado

---
+++ EjIF1/EjIF1/Ej1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EjIF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'Hola usuario'");
+            Console.Write("Ingrese su contrasena > ");
+            string contra = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'Inicio de sesion'");
+            Console.Write("Ingrese su contrasena > ");
+            string aux = Console.ReadLine();
+
+            Console.Clear();
+            if (aux.Equals(contra))
+            {
+                Console.WriteLine("Bienvenido, contrasena correcta.");
+                Console.Beep();
+            }
+            else
+            {
+                Console.WriteLine("Contrasena incorrecta, lo siento.");
+            }
+        }
+    }
+}
