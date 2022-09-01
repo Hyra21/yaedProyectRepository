73e42c16723b6861ddd3fbfee003e261c3082936
kevinopp <plata.kevin@uabc.edu.mx>
2022-09-01 10:38:33 -0700

    Terminado

---
+++ EjIF2/EjIF2/Ej2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EjIF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer numero > ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero > ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el tercer numero > ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if(num1 > num3)
+                {
+                    Console.WriteLine("El primer numero [" + num1 + "]" + " es el mayor.");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("El segundo numero [" + num2 + "]" + " es el mayor.");
+            }
+            else
+            {
+                Console.WriteLine("El tercer numero [" + num3 + "]" + " es el mayor.");
+            }
+
+        }
+    }
+}
